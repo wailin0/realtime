aa7d771697c21e620005956299bdcae0f60359c0
Wai Lin <wailinhtet.dev@gmail.com>
2024-06-20 12:18:03 +0630

    added fee

--- src/index.js
+++ src/index.js
@@ -7,20 +7,35 @@ const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 const {addUser, removeUser, getUser, getUsersInRoom} = require('./utils/users')
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3000
 const publicDir = path.join(__dirname, '../public')
 
+app.use(cors());
 app.use(express.static(publicDir))
 
 
 io.on('connection', (socket) => {
     console.log("new websocket connection")
 
+    //driver sending location
     socket.on('driverLocation', (data) => {
-        console.log(`Driver ${data.driverId} location: `, data.location);
+        console.log(`Driver ${data.driverId} sent to ${data.customerId} location: `, data.location);
         // Broadcast the driver's location to the specific customer
         io.to(data.customerId).emit('driverLocationUpdate', data);
+    });
+
+    //driver sending fee
+    socket.on('driverTripFee', (data) => {
+        console.log(`Driver ${data.driverId} trip cost: `, data.tripFee);
+        io.to(data.customerId).emit('driverTripFeeUpdate', data);
+    });
+
+    //driver sending fee
+    socket.on('tripStatus', (data) => {
+        console.log(`Driver ${data.driverId} trip status: `, data.status);
+        io.to(data.customerId).emit('tripStatusUpdate', data);
     });
 
     // Listen for the customer joining the room
@@ -29,7 +44,8 @@ io.on('connection', (socket) => {
         socket.join(roomId);
     });
 
-    // Handle client disconnection
+
+    // Handle socket client disconnection
     socket.on('disconnect', () => {
         console.log('User disconnected:', socket.id);
     });
