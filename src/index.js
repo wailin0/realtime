1a88a888f898a38b0a8941ddb3d799b999407b92
Wai Lin <wailinhtet.dev@gmail.com>
2024-06-24 11:09:30 +0630

    rest api for sending socket msh

--- src/index.js
+++ src/index.js
@@ -2,39 +2,33 @@ const express = require('express')
 const http = require('http')
 const path = require('path')
 const socketio = require('socket.io')
-const {generateMessage, generateLocationMessage} = require('./utils/messages')
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
-const {addUser, removeUser, getUser, getUsersInRoom} = require('./utils/users')
 const cors = require("cors");
 
 const PORT = process.env.PORT || 3000
 const publicDir = path.join(__dirname, '../public')
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 app.use(express.static(publicDir))
-
 
 io.on('connection', (socket) => {
     console.log("new websocket connection")
 
     //driver sending location
     socket.on('driverLocation', (data) => {
-        console.log(`Driver ${data.driverId} sent to ${data.customerId} location: `, data.location);
-        // Broadcast the driver's location to the specific customer
         io.to(data.customerId).emit('driverLocationUpdate', data);
     });
 
     //driver sending fee
-    socket.on('driverTripFee', (data) => {
-        console.log(`Driver ${data.driverId} trip cost: `, data.tripFee);
-        io.to(data.customerId).emit('driverTripFeeUpdate', data);
+    socket.on('driverTripInfo', (data) => {
+        io.to(data.customerId).emit('driverTripInfoUpdate', data);
     });
 
-    //driver sending fee
     socket.on('tripStatus', (data) => {
-        console.log(`Driver ${data.driverId} trip status: `, data.status);
         io.to(data.customerId).emit('tripStatusUpdate', data);
     });
 
@@ -51,6 +45,16 @@ io.on('connection', (socket) => {
     });
 })
 
+app.use(function(req,res,next){
+    req.io = io;
+    next();
+});
+
+app.post('/sendTripComplete', (req, res) => {
+    req.io.to(req.body.customerId).emit('tripStatusUpdate', req.body );
+
+    res.send('Message sent to all clients');
+});
 
 server.listen(PORT, () => {
     console.log('Server is up at ' + PORT)
