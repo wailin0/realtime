7d384550dbce562f17b144a34d66aba4baefb1e2
Wai Lin <wailinhtet.dev@gmail.com>
2024-08-01 18:59:59 +0630

    ad sicker

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-var express = require('express');
-var path = require('path');
+const express = require('express')
+const http = require('http')
+const path = require('path')
+const socketio = require('socket.io')
+const app = express()
 var cors = require('cors');
-
+const server = http.createServer(app)
+const io = socketio(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+})
 var indexRouter = require('./routes/index');
 var userRouter = require('./routes/user');
 var driverRouter = require('./routes/driver');
 
-var app = express();
 
 app.use(cors());
 app.use(express.json());
@@ -16,5 +24,62 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/user', userRouter);
 app.use("/driver", driverRouter);
+
+
+let driverList = {};
+
+
+io.on('connection', (socket) => {
+
+    //driver sending location
+    socket.on('driverLocation', (data) => {
+        io.to(data.customerId).emit('driverLocationUpdate', data);
+    });
+
+    socket.on('reportLocation', (data) => {
+        // data should include driverId and location info
+        driverList[data.driverId] = {socketId: socket.id, ...data};
+
+        // Broadcast the updated locations to all connected clients
+        io.emit('allDriverLocation', Object.values(driverList));
+    });
+
+    //driver sending fee
+    socket.on('driverTripInfo', (data) => {
+        io.to(data.customerId).emit('driverTripInfoUpdate', data);
+    });
+
+    socket.on('tripStatus', (data) => {
+        io.to(data.customerId).emit('tripStatusUpdate', data);
+    });
+
+    // Listen for the customer joining the room
+    socket.on('joinRoom', (roomId) => {
+        socket.join(roomId);
+    });
+
+
+    // Handle socket client disconnection
+    socket.on('disconnect', () => {
+        const driverToRemove = Object.values(driverList).find(driver => driver.socketId === socket.id);
+        if (driverToRemove) {
+            delete driverList[driverToRemove.driverId];
+            // Broadcast the updated driverList to all connected clients after removal
+            io.emit('allDriverLocation', Object.values(driverList));
+        }
+    });
+})
+
+app.use(function (req, res, next) {
+    req.io = io;
+    next();
+});
+
+app.post('/sendTripStatus', (req, res) => {
+    req.io.to(req.body.customerId).emit('tripStatusUpdate', req.body);
+
+    res.send('Message sent to client');
+});
+
 
 module.exports = app;
